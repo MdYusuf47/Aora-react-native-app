11a2967faf68fdc48c05136a2acf220c62210754
Md Yusuf <mdyusuf742171@gmail.com>
2024-05-07 12:14:10 +0530

    setup folder structure

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const sSignIn = () => {
+  return (
+    <View>
+      <Text>sSignIn</Text>
+    </View>
+  )
+}
+
+export default sSignIn
